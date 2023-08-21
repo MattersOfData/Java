1fbc437dc967bb9129100b5025e3ee6757d0ccbb
MattersOfData <myshin.data@gmail.com>
2023-08-21 14:37:05 +0900

    Chapter6_Example

---
+++ JavaJungsuk/src/ch06/Ex6_11.java
@@ -0,0 +1,29 @@
+package ch06;
+
+
+class Data_1{
+	int value;
+	// 기본 생성자 자동 추가
+}
+class Data_2{
+	int value;
+	
+	Data_2() {} // 기본 생성자 수동 추가
+	Data_2(int x){// 메게뱐스기 있는 생성자
+		value = x;
+	}
+		
+}
+
+public class Ex6_11 {
+	public static void main(String[] args) {
+	Data_1 d1  = new Data_1();
+	Data_2 d2  = new Data_2();
+	
+	}
+	
+}
+
+
+		
+	
