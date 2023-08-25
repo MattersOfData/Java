fd1f5df30105a1319dd5f13a57d53ca6cdcbf74b
MattersOfData <myshin.data@gmail.com>
2023-08-25 23:49:45 +0900

    Chapter2_Example

--- JavaJungsuk/src/ch02/VarEx1.java
+++ JavaJungsuk/src/ch02/VarEx1.java
@@ -3,23 +3,24 @@ package ch02;
 public class VarEx1 {
 
 	public static void main(String[] args) {
-	System.out.println("6+3"); // "" 안에 있는 내용 그대로 출력
 	
-	System.out.println(6+3); // 덧셈
-	System.out.println(6-3); // 뺼셈
-	System.out.println(6*3); // 곱셈
-	System.out.println(6/3); // 나눗셈
-	
-	// 값을 일일이 적어주기보다, 변수를 선언 & 이를 이용하면, 다른 결과값 쉽게 도출 가능 
-	// 변수의 필요 이유!
-	
-	int x = 6, y = 3;
-	System.out.println(x+y); // 덧셈
-	System.out.println(x-y); // 뺄셈
-	System.out.println(x*y); // 곱셈
-	System.out.println(x/y); // 나눗셈
-	
-	
+		System.out.println("6+3"); // "" 안에 있는 내용 그대로 출력
+		
+		System.out.println(6+3); // 덧셈
+		System.out.println(6-3); // 뺼셈
+		System.out.println(6*3); // 곱셈
+		System.out.println(6/3); // 나눗셈
+		
+		// 값을 일일이 적어주기보다, 변수를 선언 & 이를 이용하면, 다른 결과값 쉽게 도출 가능 
+		// 변수의 필요 이유!
+		
+		int x = 6, y = 3;
+		System.out.println(x+y); // 덧셈
+		System.out.println(x-y); // 뺄셈
+		System.out.println(x*y); // 곱셈
+		System.out.println(x/y); // 나눗셈
+		
+					
 	}
 
 }
--- JavaJungsuk/src/ch02/VarEx3.java
+++ JavaJungsuk/src/ch02/VarEx3.java
@@ -13,6 +13,32 @@ public class VarEx3 {
 		final int MAX = 100;
 		System.out.println("MAX = " + MAX);
 		//MAX = 200;  MAX는 상수임으로, 처음 값에서 다른값으로 변경할수 X
+		
+		boolean power = true; // or false
+		System.out.println(power);
+		
+		// byte b = 128; byte 범위 : -128 ~127
+		byte b = 127;
+		System.out.println(b);
+		
+		int oct = 010; // 8진수 -> 10진수로 8
+		int hex = 0x10; //16진수 -> 10진수로 16
+		// println 은 10진수로만 출력 가능
+		System.out.println(oct); // 8
+		System.out.println(hex); // 16
+		
+		long l = 1000_000_000; // 10억은 int -> long 타입에 저장 가능 
+		// long l = 3_000_000_000; 30억은 int 타입으로 저장할 수 없음
+		long l2 = 3_000_000_000L ; // long 타입으로 접미사 'L' 붙여서 저장해줘야 함 !
+		
+		float f = 3.14f; // float 타입은 필수로 접미사 f !
+		double d = 3.14; // double 타입은 접미사 d 생략 가능 
+		double d2 = 3.14f; // 에러 발생 x -> double> float
+		
+		System.out.println(10.); // 10.0
+		System.out.println(0.10); // 0.1 
+		System.out.println(10f); // 10.0
+		System.out.println(1e3); // 1000.0
 	}
 
 }
