6a68e2facdaae98fecc613d3031532b86545015a
MattersOfData <myshin.data@gmail.com>
2023-08-31 21:13:03 +0900

    Chapter3_Exercise3_5

---
+++ JavaJungsuk/src/ch03/Ex3_5.java
@@ -0,0 +1,16 @@
+package ch03;
+
+public class Ex3_5 {
+
+	public static void main(String[] args) {
+		//변수 num의 일의 자리 수를 1로 바꾸는 코드
+		int num = 777;  
+		int num2 = num/10;
+		int num3 = (num2 * 10);
+		System.out.println(num3 +1);
+		
+		System.out.println(num/10*10+1);
+		}
+	}
+
+
