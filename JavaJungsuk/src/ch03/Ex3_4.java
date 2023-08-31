405b909dbd28446ad3e5da60b08df28dae659098
MattersOfData <myshin.data@gmail.com>
2023-08-31 19:45:07 +0900

    Chapter3_Exercise3_4

---
+++ JavaJungsuk/src/ch03/Ex3_4.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class Ex3_4 {
+
+	public static void main(String[] args) {
+		
+		int num = 123;
+		int result = num/100 ; // int /int = int(나머지 버림) -> 456/100 =4
+		System.out.println(result *100);
+		
+		}
+	}
+
+
