1e78083dc05ae31bf65a242c3ed1c83c34acc969
MattersOfData <myshin.data@gmail.com>
2023-09-07 16:02:54 +0900

    Ex4_5

---
+++ JungsukCoding/src/test_0907/Ex4_5.java
@@ -0,0 +1,20 @@
+package test_0907;
+
+public class Ex4_5 {
+
+	public static void main(String[] args) {
+	// for문을 while 문으로 변경하기
+		// for문
+		for(int i = 0; i<=10; i++) {
+			System.out.print("*");
+		}System.out.println();
+	
+		// while문
+		int i = 0;
+		while(i<=10) {
+			System.out.print("*");
+			i++;
+		}System.out.println();
+	}
+
+}
