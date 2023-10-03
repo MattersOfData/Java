1c33e608bf7c7f9d722870eddab0a12fb6ddcc56
MattersOfData <myshin.data@gmail.com>
2023-10-03 23:19:03 +0900

    Ex11_5

---
+++ JungsukCoding/src/test_1002/Ex11_5.java
@@ -0,0 +1,58 @@
+package test_1002;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Ex11_5 {
+	
+		public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		
+		list.add(new Student("홍길동",1,1,100,100,100)); 
+		list.add(new Student("남궁성 ",1,2,90,70,80)); 
+		list.add(new Student("김자바 ",1,3,80,80,90));
+		list.add(new Student("이자바 ",1,4,70,90,70)); 
+		list.add(new Student("안자바 ",1,5,60,100,80));
+		
+		Collections.sort(list); // list에 저장된 요소들을 정렬 with compareTo() 
+		Iterator it = list.iterator();
+		
+		while(it.hasNext())
+			System.out.println(it.next());
+		
+		}	
+	}
+		class Student implements Comparable {
+			
+			String name;
+			int ban;
+			int no;
+			int kor, eng, math;
+			
+			Student(String name, int ban, int no, int kor, int eng, int math) {
+			this.name = name;
+			this.ban = ban;
+			this.no = no;
+			this.kor = kor;
+			this.eng = eng;
+			this.math = math;
+				}
+			int getTotal() {
+			return kor+eng+math;
+				}
+			float getAverage() {
+			return (int)((getTotal()/ 3f)*10+0.5)/10f;
+				}
+			
+			public int compareTo(Object o) {
+				if(o instanceof Student) {// 형변환 가능 확인
+					Student tmp = (Student)o; // 형변환
+				return name.compareTo(tmp.name); // iv와 tmp가 가르키는 name 비교
+				}return -1; // 형변환 불가한경우 , -1 return
+			}
+			public String toString() {
+			return name +","+ban +","+no +","+kor +","+eng +","+math
+			+","+getTotal() +","+getAverage();
+			}
+		}
