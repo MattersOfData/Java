5061133894cc3ab61840d78598f98c2c738dd6e7
MattersOfData <myshin.data@gmail.com>
2023-10-20 16:36:38 +0900

    Ex11_10

--- JavaJungsuk/src/ch11/Ex11_10.java
+++ JavaJungsuk/src/ch11/Ex11_10.java
@@ -14,16 +14,17 @@ public class Ex11_10 {
 		
 		Stack st = new Stack();
 		String expression = "((3+5)*8-2)"; // 1. 여는 괄호 2. 여는괄호 3. 닫는괄호(스택에서 뻄) 4. 닫는괄호(스택에서 뺌) -> 스택비워지면 괄호 OK
+										   // "((3+5)*8-2))))"; 스택에서 꺼낼 괄호가 없는 경우 
 		//String expression = args[0]; // 위에 if 식 사용할꺼면-> run configuration에서 arguments에서 수식입력 필요 !
 		System.out.println("expression:" + expression);
-
+		
 		try {
 			for(int i = 0; i< expression.length(); i++) {
 				char ch = expression.charAt(i); // expression 문자열에서 하나씩 꺼냄
 				
 				if(ch =='(' ) { // 여는 괄호라면
 					st.push(ch+""); // 넣기
-				}else if(ch== '(' ) {
+				}else if(ch== ')' ) {
 					st.pop(); // 꺼내기
 				}
 			} // end of for
