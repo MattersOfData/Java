837b414a83804f763b3b8dc09de434b2b54217f2
MattersOfData <myshin.data@gmail.com>
2023-10-24 02:58:32 +0900

    Ex11_30

---
+++ JavaJungsuk/src/ch11/Ex11_30.java
@@ -0,0 +1,43 @@
+package ch11;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Ex11_30 {
+// HashMap 예제 with containsKey, equals 
+	
+	public static void main(String[] args) {
+		HashMap map = new HashMap();
+		map.put("myId", "1234");
+		map.put("asdf", "1111");
+		System.out.println(map); // {myId=1234, asdf=1111}
+		map.put("asdf", "1234"); // 키가 같을경우, 마지막 값으로 덮어씀
+		System.out.println(map); // {myId=1234, asdf=1234}
+		 
+		Scanner s = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("id와 pw를 입력해주세요");
+			System.out.println("id :");
+			String id = s.nextLine().trim(); // " asdf   " -> "asdf" : 앞뒤 공백 제거
+			
+			System.out.println("pw :");
+			String password = s.nextLine().trim(); // " 1234   " -> "1234" : 앞뒤 공백 제거
+			System.out.println();
+			
+			if(!(map.containsKey(id))) {
+				System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요");
+				continue; // while로 되돌아감
+			}
+			
+			if(!(map.get(id).equals(password))) { // map.get(id) = "1234"
+				System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
+			}else {
+				System.out.println("id와 비밀번호가 일치합니다");
+				break; // while 빠져나옴
+			}
+			
+		}// while
+	}//main
+
+}
